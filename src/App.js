bcade1aee56f8533effeb83e5535d81220e5833b
ModakSanika <sanikamodak29@gmail.com>
2023-09-25 21:35:28 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
+let currDate= new Date();
+currDate=currDate.getHours();
+let greeting='';
+const cssStyle={};
+
+if (currDate >=1&& currDate<12){
+  greeting="Good Morning!!"
+  cssStyle.color='green'
+}else if(currDate>12 && currDate<16){
+  greeting="Good Afternoon!!"
+  cssStyle.color='Orange'
+}
+else{
+  greeting=" Good Evening!!"
+  cssStyle.color='violet'
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+    <h1>Hello Sir, <span style={cssStyle}>{greeting} </span> </h1>
     </div>
+    </>
+  
   );
 }
 
